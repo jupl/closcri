82e91d022eae778868b9d3cb8b2646f7ef018c54
jupl <juan.placencia.512@gmail.com>
2016-05-14 16:56:15 -0500

    Move eletron references to the top

--- src/app/electron.cljs
+++ src/app/electron.cljs
@@ -4,12 +4,13 @@
             [camel-snake-kebab.extras :refer [transform-keys]]))
 
 (def electron (js/require "electron"))
+(def app (.-app electron))
+(def browser-window (.-BrowserWindow electron))
 
 (def window (atom nil))
 
 (defn new-window [& args]
-  (let [browser-window (.-BrowserWindow electron)
-        args-map (apply hash-map args)
+  (let [args-map (apply hash-map args)
         camel-args-map (transform-keys ->camelCaseString args-map)
         options (clj->js camel-args-map)]
     (browser-window. options)))
@@ -22,9 +23,8 @@
   (.focus @window))
 
 (defn init []
-  (let [app (.-app electron)]
-    (.on app "ready" #(init-window window))
-    (.on app "activate" #(init-window window))
-    (.on app "window-all-closed" #(when-not config/osx (.quit app)))))
+  (.on app "ready" #(init-window main-window))
+  (.on app "activate" #(init-window main-window))
+  (.on app "window-all-closed" #(when-not config/osx (.quit app))))
 
 (set! *main-cli-fn* identity)
