b386e9dfaba7aaf5c5e0e8f41f1abda224b9359e
jupl <juan.placencia.512@gmail.com>
2016-05-14 12:48:31 -0500

    Formatting

--- src/app/config.cljs
+++ src/app/config.cljs
@@ -2,12 +2,12 @@
 
 (goog-define production false)
 
-(def osx (= js/process.platform "darwin"))
-
 (def base-url (if production
                 (str "file://" js/__dirname "/")
                 (let [os (js/require "os")]
                   (str "http://" (.hostname os) ":3000/"))))
+
+(def osx (= js/process.platform "darwin"))
 
 (when-not production
   (enable-console-print!))
