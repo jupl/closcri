188171547e174cede64ec5ed62b410438e85f75e
jupl <juan.placencia.512@gmail.com>
2016-05-14 11:42:17 -0500

    Refactor code

--- src/app/config.cljs
+++ src/app/config.cljs
@@ -4,5 +4,11 @@
 
 (def osx (= js/process.platform "darwin"))
 
+(def base-url
+  (if production
+    (str "file://" js/__dirname "/")
+    (let [os (js/require "os")]
+      (str "http://" (.hostname os) ":3000/"))))
+
 (when-not production
   (enable-console-print!))
--- src/app/electron.cljs
+++ src/app/electron.cljs
@@ -4,11 +4,7 @@
             [camel-snake-kebab.extras :refer [transform-keys]]))
 
 (def electron (js/require "electron"))
-(def os (js/require "os"))
 
-(def base-url (if config/production
-                (str "file://" js/__dirname "/")
-                (str "http://" (.hostname os) ":3000/")))
 (def window (atom nil))
 
 (defn new-window [& args]
@@ -18,15 +14,16 @@
         options (clj->js camel-args-map)]
     (browser-window. options)))
 
-(defn init-window []
-  (reset! window (new-window))
-  (.loadURL @window (str base-url "index.html"))
-  (.on @window "closed" #(reset! window nil)))
+(defn init-window [window & {:keys [url] :or {url "index.html"}}]
+  (when (nil? @window)
+    (reset! window (new-window))
+    (.loadURL @window (str config/base-url url))
+    (.on @window "closed" #(reset! window nil))))
 
 (defn init []
   (let [app (.-app electron)]
-    (.on app "ready" init-window)
-    (.on app "activate" #(when (nil? @window) (init-window)))
+    (.on app "ready" #(init-window window))
+    (.on app "activate" #(init-window window))
     (.on app "window-all-closed" #(when-not config/osx (.quit app)))))
 
 (set! *main-cli-fn* identity)
