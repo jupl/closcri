c9b983099a52fbf06c2d9887aaf645e2a42076af
jupl <juan.placencia.512@gmail.com>
2016-05-16 21:07:05 -0500

    Add initial version of Electron menu support

---
+++ src/app/electron/menu.cljs
@@ -0,0 +1,39 @@
+(ns app.electron.menu
+  (:require [app.config :as config]
+            [app.electron.window :refer [init-window]]))
+
+(def electron (js/require "electron"))
+(def app (.-app electron))
+(def menu (.-Menu electron))
+
+(def menu-template (atom []))
+(def osx-menu-item {:label (.getName app)
+                    :submenu [{:label "Quit"
+                               :accelerator "Command+Q"
+                               :click #(.quit app)}]})
+
+(if-not config/production
+  (let [devcards-window (atom nil)
+        open-devcards #(init-window devcards-window "devcards.html")
+        toggle-devtools #(.toggleDevTools %2)]
+    (def dev-menu-item {:label "Develop"
+                        :submenu [{:label "Toggle Developer Tools"
+                                   :accelerator "F12"
+                                   :click toggle-devtools}
+                                  {:label "Open Devcards"
+                                   :accelerator "Shift+F12"
+                                   :click open-devcards}]})))
+
+(let [pre-menu (if config/osx [osx-menu-item] [])
+      post-menu (if config/production [] [dev-menu-item])]
+  (defn update-menu! [_ _ _ menu-template]
+    (as-> menu-template x
+      (into pre-menu x)
+      (into x post-menu)
+      (clj->js x)
+      (.buildFromTemplate menu x)
+      (.setApplicationMenu menu x))))
+
+;; (.on app "ready" (fn []
+;;                    (add-watch menu-template :watcher update-menu!)
+;;                    (reset! menu-template @menu-template)))
