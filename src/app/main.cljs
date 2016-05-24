c0d3402749a6f25c6a4ae5754f173701247c90a0
jupl <juan.placencia.512@gmail.com>
2016-05-24 07:38:59 -0500

    Simplify base code

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,16 +1,9 @@
 (ns app.main
   (:require [app.config :as config]))
 
-(defonce load-time (.toLocaleString (js/Date.)))
-
 (defn render []
   (set! js/container.style.backgroundColor "gainsboro")
-  (let [update-time (.toLocaleString (js/Date.))
-        message (if config/production
-                  (str "Loaded at " load-time ".")
-                  (str "Loaded at " load-time ". "
-                       "Updated at " update-time "."))]
-    (set! js/container.innerHTML message)))
+  (set! js/container.innerHTML "Hello world"))
 
 (defn init []
   (when config/production
