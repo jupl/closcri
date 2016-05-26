fe35080ce11a90b1cf1f54b689fcca40c3bfc421
jupl <juan.placencia.512@gmail.com>
2016-05-26 00:04:12 -0400

    Refactor

--- src/electron/menu.cljs
+++ src/electron/menu.cljs
@@ -12,27 +12,29 @@
                                :accelerator "Command+Q"
                                :click #(.quit app)}]})
 
-(if-not config/production
-  (let [devcards-window (atom nil)
-        open-devcards #(init-window devcards-window "devcards.html")
-        toggle-devtools #(.toggleDevTools %2)]
-    (def dev-menu-item {:label "Develop"
-                        :submenu [{:label "Toggle Developer Tools"
-                                   :accelerator "F12"
-                                   :click toggle-devtools}
-                                  {:label "Open Devcards"
-                                   :accelerator "Shift+F12"
-                                   :click open-devcards}]})))
+(def dev-menu-item
+  (when-not config/prouction
+    (let [devcards-window (atom nil)
+          open-devcards #(init-window devcards-window "devcards.html")
+          toggle-devtools #(.toggleDevTools %2)]
+      {:label "Develop"
+       :submenu [{:label "Toggle Developer Tools"
+                  :accelerator "F12"
+                  :click toggle-devtools}
+                 {:label "Open Devcards"
+                  :accelerator "Shift+F12"
+                  :click open-devcards}]})))
 
-(let [pre-menu (if config/osx [osx-menu-item] [])
-      post-menu (if config/production [] [dev-menu-item])]
-  (defn update-menu! [_ _ _ menu-template]
-    (as-> menu-template x
-      (into pre-menu x)
-      (into x post-menu)
-      (clj->js x)
-      (.buildFromTemplate menu x)
-      (.setApplicationMenu menu x))))
+(def update-menu!
+  (let [pre-menu (if config/osx [osx-menu-item] [])
+        post-menu (if config/production [] [dev-menu-item])]
+    (fn [_ _ _ menu-template]
+      (as-> menu-template x
+        (into pre-menu x)
+        (into x post-menu)
+        (clj->js x)
+        (.buildFromTemplate menu x)
+        (.setApplicationMenu menu x)))))
 
 (.on app "ready" (fn []
                    (add-watch menu-template :watcher update-menu!)
