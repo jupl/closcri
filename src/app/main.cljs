f352ab8741f7acef602afad9a16dd1a19d9ff8da
jupl <juan.placencia.512@gmail.com>
2016-10-25 00:13:01 -0500

    Fix merge

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,29 +1,10 @@
 (ns app.main
-  (:require [core.config :as config]
-            [core.reload :as reload]))
+  "Entry point for application."
+  (:require
+   [common.config :as config]))
 
-(def container-style
-  "Style attributes applied to the CLJS application container"
-  {:position "fixed"
-   :top 0
-   :bottom 0
-   :left 0
-   :right 0
-   :overflow "auto"
-   :background-color "white"})
-
-(defn render
-  "Render the application."
+(defn main
+  "Application entry point."
   []
-  (set! js/container.style.cssText nil)
-  (doseq [[key val] container-style]
-    (aset js/container.style (clj->js key) (clj->js val)))
-  (set! js/container.innerHTML "Hello world"))
-
-(defn init
-  "Configure and bootstrap the application."
-  []
-  (when (identical? config/production false)
-    (enable-console-print!)
-    (reload/add-handler #'render))
-  (render))
+  ;; Take out that nil and start writing!
+  nil)
--- src/core/config.cljs
+++
@@ -1,4 +0,0 @@
-(ns core.config)
-
-;; If true then application is a production environment
-(goog-define production false)
